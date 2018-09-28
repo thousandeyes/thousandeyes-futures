3501fc8965155fea68582e2e93c3a5187616f25b
Giannis Georgalis <ggeorgalis@thousandeyes.com>
2018-09-28 17:23:28 +0100

    EAC-912 Copied thousandeyes-futures from eyebrow repo

---
+++ include/thousandeyes/futures/TimedWaitable.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <chrono>
+#include <string>
+#include <stdexcept>
+
+#include <thousandeyes/futures/Waitable.h>
+
+namespace thousandeyes {
+namespace futures {
+
+//! \brief Exception thrown by the TimedWaitable objects when they time out.
+//!
+//! \sa TimedWaitable
+class WaitableTimedOutException : public std::runtime_error {
+public:
+    explicit WaitableTimedOutException(const std::string& error) :
+        std::runtime_error(error)
+    {}
+};
+
+//! \brief A Waitable Interface to represent objects that can be waited on, that
+//! can time-out and, finally, that can get dispatched.
+class TimedWaitable : public Waitable {
+public:
+    //! \brief Creates a TimedWaitable object whose wait() method throws a
+    //! WaitableTimedOutException if it has been called with timeouts that add
+    //! up to or exceed the object's waitLimit.
+    //!
+    //! \param waitLimit The maximum time that the wait() method's timeouts
+    //! can add up to before the object is considered timed-out.
+    explicit TimedWaitable(std::chrono::microseconds waitLimit) :
+        waitLimit_(std::move(waitLimit))
+    {}
+
+    //! \throws WaitableTimedOutException if the object has timed-out as a result
+    //! of multiple invocations of the wait() method with timeouts that add
+    //! up to or exceed the object's waitLimit.
+    //!
+    //! \note Objects that inherit the TimedWaitable Interface should override
+    //! timedWait() to implement their specific waiting logic.
+    //!
+    //! \sa timedWait()
+    bool wait(const std::chrono::microseconds& timeout) override final
+    {
+        if (waitLimit_ <= std::chrono::microseconds(0)) {
+            throw WaitableTimedOutException("Wait limit exceeded");
+        }
+
+        if (timedWait(timeout)) {
+            return true;
+        }
+
+        waitLimit_ -= timeout;
+        return false;
+    }
+
+    //! \brief Waits, at most, the given amount of time to determine whether
+    //! the object is fulfilled or not.
+    //!
+    //! \param timeout The maximum time to wait until determining whether the object
+    //! is fulfilled or not.
+    //!
+    //! \return true if the object is fulfilled and false otherwise.
+    //!
+    //! \throws std::exception if something exceptional happens during waiting.
+    //!
+    //! \note If timedWait() returns true, subsequent invocations of timedWait()
+    //! should also return true as soon as possible.
+    virtual bool timedWait(const std::chrono::microseconds& timeout) = 0;
+
+protected:
+    std::chrono::microseconds getWaitLimit() const
+    {
+        return waitLimit_;
+    }
+
+private:
+    std::chrono::microseconds waitLimit_;
+};
+
+} // namespace futures
+} // namespace thousandeyes
