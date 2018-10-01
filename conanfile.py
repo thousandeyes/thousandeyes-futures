07cd998a7b68fbe596caa98299b07d53e7d50012
Giannis Georgalis <ggeorgalis@thousandeyes.com>
2018-10-01 15:45:59 +0100

    EAC-913 Added FindThousandEyesFutures.cmake file

--- conanfile.py
+++ conanfile.py
@@ -3,11 +3,12 @@ from conans import ConanFile, CMake
 class ThousandEyesFuturesConan(ConanFile):
     name = "thousandeyes-futures"
     version = "0.1"
-    exports_sources = "include/*"
+    exports_sources = "include/*", "FindThousandEyesFutures.cmake"
     no_copy_source = True
 
     def package(self):
         self.copy("*.h")
+        self.copy("FindThousandeyesFutures.cmake", dst=".", src=".")
 
     def package_id(self):
         self.info.header_only()
