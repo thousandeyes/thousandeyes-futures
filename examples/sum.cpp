901c671be94a69650805ee85693188c9209c8fc4
Giannis Georgalis <ggeorgalis@thousandeyes.com>
2018-11-12 11:30:33 +0000

    EAC-930 Added examples

---
+++ examples/sum.cpp
@@ -0,0 +1,51 @@
+#include <functional>
+#include <future>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <memory>
+
+#include <thousandeyes/futures/DefaultExecutor.h>
+#include <thousandeyes/futures/all.h>
+#include <thousandeyes/futures/then.h>
+
+using namespace std;
+using namespace std::chrono;
+using namespace thousandeyes::futures;
+
+namespace {
+
+template<class T>
+future<T> getValueAsync(const T& value)
+{
+    return std::async(std::launch::async, [value]() {
+        return value;
+    });
+}
+
+} // namespace
+
+int main(int argc, const char* argv[])
+{
+    auto executor = make_shared<DefaultExecutor>(milliseconds(10));
+    Default<Executor>::Setter execSetter(executor);
+
+    vector<future<int>> futures;
+    for (int i = 0; i < 1821; ++i) {
+        futures.push_back(getValueAsync(i));
+    }
+
+    auto f = then(all(move(futures)), [](future<vector<future<int>>> f) {
+        auto futures = f.get();
+
+        return accumulate(futures.begin(), futures.end(), 0, [](int sum, future<int>& f) {
+            return sum + f.get();
+        });
+    });
+
+    int result = f.get();
+
+    cout << "Got result: " << result << endl;
+
+    executor->stop();
+}
